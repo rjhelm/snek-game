e3efc05cfabbc44bd4ce2a2bc2c155b17f92d51f
savbennett8 <email@example.com>
2021-03-09 22:40:33 -0700

    gif setup in modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,28 +9,28 @@ var modalBtn = document.getElementById("modalBtn");
 var food = document.getElementById("food");
 var randomObstacle = document.getElementById("randomObstacle");
 var snakeHeadX,
-    snakeHeadY,
-    foodX,
-    foodY,
-    randomObstacleX,
-    randomObstacleY,
-    tail,
-    totalTail,
-    directionVar,
-    direction,
-    previousDir;
+  snakeHeadY,
+  foodX,
+  foodY,
+  randomObstacleX,
+  randomObstacleY,
+  tail,
+  totalTail,
+  directionVar,
+  direction,
+  previousDir;
 var speed = 1,
-    xSpeed,
-    ySpeed;
+  xSpeed,
+  ySpeed;
 const scale = 20;
 var rows = canvas.height / scale;
 var columns = canvas.width / scale;
 var min = scale / 10;
 var max = rows - min;
 var gameInterval,
-    randObstacleInterval,
-    intervalDuration = 150,
-    minDuration = 75;
+  randObstacleInterval,
+  intervalDuration = 150,
+  minDuration = 75;
 var playing, gameStarted;
 var boundaryCollision;
 var tail0;
@@ -39,22 +39,22 @@ startBtn.addEventListener("click", startGame);
 
 // Reset values of variables to starting values]
 function reset() {
-    clearInterval(gameInterval);
-    clearInterval(randObstacleInterval);
-    (intervalDuration = 150), (minDuration = 75);
-    tail = [];
-    totalTail = 0;
-    directionVar = "Right";
-    direction = "Right";
-    previousDir = "Right";
-    xSpeed = scale * speed;
-    ySpeed = 0;
-    snakeHeadX = 0;
-    snakeHeadY = 0;
-    pauseBtn.style.backgroundColor = "#fff";
-    resumeBtn.style.backgroundColor = "#fff";
-    (playing = false), (gameStarted = false);
-    boundaryCollision = false;
+  clearInterval(gameInterval);
+  clearInterval(randObstacleInterval);
+  (intervalDuration = 150), (minDuration = 75);
+  tail = [];
+  totalTail = 0;
+  directionVar = "Right";
+  direction = "Right";
+  previousDir = "Right";
+  xSpeed = scale * speed;
+  ySpeed = 0;
+  snakeHeadX = 0;
+  snakeHeadY = 0;
+  pauseBtn.style.backgroundColor = "#fff";
+  resumeBtn.style.backgroundColor = "#fff";
+  (playing = false), (gameStarted = false);
+  boundaryCollision = false;
 }
 // Start the game 
 function startGame() {
@@ -176,158 +176,158 @@ function generateCoordinates() {
 }
 
 function drawSnakeHead(color) {
-    context.beginPath();
+  context.beginPath();
+  context.arc(
+    snakeHeadX + scale / 2,
+    snakeHeadY + scale / 2,
+    scale / 2,
+    0,
+    2 * Math.PI
+  );
+  context.fillStyle = color;
+  context.fill();
+  //eyes
+  context.beginPath();
+  if (direction === "Up") {
     context.arc(
-        snakeHeadX + scale / 2,
-        snakeHeadY + scale / 2,
-        scale / 2,
-        0,
-        2 * Math.PI
+      snakeHeadX + scale / 5,
+      snakeHeadY + scale / 5,
+      scale / 8,
+      0,
+      2 * Math.PI
     );
-    context.fillStyle = color;
-    context.fill();
-    //eyes
-    context.beginPath();
-    if (direction === "Up") {
-        context.arc(
-            snakeHeadX + scale / 5,
-            snakeHeadY + scale / 5,
-            scale / 8,
-            0,
-            2 * Math.PI
-        );
-        context.arc(
-            snakeHeadX + scale - scale / 5,
-            snakeHeadY + scale / 5,
-            scale / 8,
-            0,
-            2 * Math.PI
-        );
-    } else if (direction === "Down") {
-        context.arc(
-            snakeHeadX + scale / 5,
-            snakeHeadY + scale - scale / 5,
-            scale / 8,
-            0,
-            2 * Math.PI
-        );
-        context.arc(
-            snakeHeadX + scale - scale / 5,
-            snakeHeadY + scale - scale / 5,
-            scale / 8,
-            0,
-            2 * Math.PI
-        );
-    } else if (direction === "Left") {
-        context.arc(
-            snakeHeadX + scale / 5,
-            snakeHeadY + scale / 5,
-            scale / 8,
-            0,
-            2 * Math.PI
-        );
-        context.arc(
-            snakeHeadX + scale / 5,
-            snakeHeadY + scale - scale / 5,
-            scale / 8,
-            0,
-            2 * Math.PI
-        );
-    } else {
-        context.arc(
-            snakeHeadX + scale - scale / 5,
-            snakeHeadY + scale / 5,
-            scale / 8,
-            0,
-            2 * Math.PI
-        );
-        context.arc(
-            snakeHeadX + scale - scale / 5,
-            snakeHeadY + scale - scale / 5,
-            scale / 8,
-            0,
-            2 * Math.PI
-        );
-    }
-    context.fillStyle = "black";
-    context.fill();
+    context.arc(
+      snakeHeadX + scale - scale / 5,
+      snakeHeadY + scale / 5,
+      scale / 8,
+      0,
+      2 * Math.PI
+    );
+  } else if (direction === "Down") {
+    context.arc(
+      snakeHeadX + scale / 5,
+      snakeHeadY + scale - scale / 5,
+      scale / 8,
+      0,
+      2 * Math.PI
+    );
+    context.arc(
+      snakeHeadX + scale - scale / 5,
+      snakeHeadY + scale - scale / 5,
+      scale / 8,
+      0,
+      2 * Math.PI
+    );
+  } else if (direction === "Left") {
+    context.arc(
+      snakeHeadX + scale / 5,
+      snakeHeadY + scale / 5,
+      scale / 8,
+      0,
+      2 * Math.PI
+    );
+    context.arc(
+      snakeHeadX + scale / 5,
+      snakeHeadY + scale - scale / 5,
+      scale / 8,
+      0,
+      2 * Math.PI
+    );
+  } else {
+    context.arc(
+      snakeHeadX + scale - scale / 5,
+      snakeHeadY + scale / 5,
+      scale / 8,
+      0,
+      2 * Math.PI
+    );
+    context.arc(
+      snakeHeadX + scale - scale / 5,
+      snakeHeadY + scale - scale / 5,
+      scale / 8,
+      0,
+      2 * Math.PI
+    );
+  }
+  context.fillStyle = "black";
+  context.fill();
 }
 
 function drawSnakeTail() {
-    let tailRadius = scale / 4;
-    for (i = 0; i < tail.length; i++) {
-        tailRadius = tailRadius + (scale / 2 - scale / 4) / tail.length;
-        context.beginPath();
-        context.fillStyle = "#6c2c3a";
-        context.arc(
-            tail[i].tailX + scale / 2,
-            tail[i].tailY + scale / 2,
-            tailRadius,
-            0,
-            2 * Math.PI
-        );
-        context.fill();
-    }
+  let tailRadius = scale / 4;
+  for (i = 0; i < tail.length; i++) {
+    tailRadius = tailRadius + (scale / 2 - scale / 4) / tail.length;
+    context.beginPath();
+    context.fillStyle = "#6c2c3a";
+    context.arc(
+      tail[i].tailX + scale / 2,
+      tail[i].tailY + scale / 2,
+      tailRadius,
+      0,
+      2 * Math.PI
+    );
+    context.fill();
+  }
 }
 
 //shift snake's previous positions to next position
 function moveSnakeForward() {
-    tail0 = tail[0];
-    for (let i = 0; i < tail.length - 1; i++) {
-        tail[i] = tail[i + 1];
-    }
-    tail[totalTail - 1] = { tailX: snakeHeadX, tailY: snakeHeadY };
-    snakeHeadX += xSpeed;
-    snakeHeadY += ySpeed;
+  tail0 = tail[0];
+  for (let i = 0; i < tail.length - 1; i++) {
+    tail[i] = tail[i + 1];
+  }
+  tail[totalTail - 1] = { tailX: snakeHeadX, tailY: snakeHeadY };
+  snakeHeadX += xSpeed;
+  snakeHeadY += ySpeed;
 }
 
 //only in case of boundary collision
 function moveSnakeBack() {
-    context.clearRect(0, 0, 500, 500);
-    for (let i = tail.length - 1; i >= 1; i--) {
-        tail[i] = tail[i - 1];
-    }
-    if (tail.length >= 1) {
-        tail[0] = { tailX: tail0.tailX, tailY: tail0.tailY };
-    }
-    snakeHeadX -= xSpeed;
-    snakeHeadY -= ySpeed;
-    drawRandomObstacle();
-    drawFood();
-    drawSnakeTail();
+  context.clearRect(0, 0, 500, 500);
+  for (let i = tail.length - 1; i >= 1; i--) {
+    tail[i] = tail[i - 1];
+  }
+  if (tail.length >= 1) {
+    tail[0] = { tailX: tail0.tailX, tailY: tail0.tailY };
+  }
+  snakeHeadX -= xSpeed;
+  snakeHeadY -= ySpeed;
+  drawRandomObstacle();
+  drawFood();
+  drawSnakeTail();
 }
 
 //display snake
 function drawSnake() {
-    drawSnakeHead("#7d4350");
-    drawSnakeTail();
-    if (checkCollision()) {
-        clearInterval(gameInterval);
-        clearInterval(randObstacleInterval);
-        if (boundaryCollision) {
-            moveSnakeBack();
-        }
-        drawSnakeHead("red");
-        setTimeout(() => {
-            scoreModal.textContent = totalTail;
-            $("#alertModal").modal("show");
-            //if modal is shown, remove the keydown event listener so that snake doesn't move
-            $("#alertModal").on("shown.bs.modal", function () {
-                window.removeEventListener("keydown", pressedKey);
-            });
-            //when modal hides, reset every variable and add keydown event listener again
-            $("#alertModal").on("hidden.bs.modal", function () {
-                context.clearRect(0, 0, 500, 500);
-                score.innerText = 0;
-                window.addEventListener("keydown", pressedKey);
-                reset();
-            });
-            modalBtn.addEventListener("click", () => {
-                context.clearRect(0, 0, 500, 500);
-                score.innerText = 0;
-            });
-        }, 1000);
+  drawSnakeHead("#7d4350");
+  drawSnakeTail();
+  if (checkCollision()) {
+    clearInterval(gameInterval);
+    clearInterval(randObstacleInterval);
+    if (boundaryCollision) {
+      moveSnakeBack();
     }
+    drawSnakeHead("red");
+    setTimeout(() => {
+      scoreModal.textContent = totalTail;
+      $("#alertModal").modal("show");
+      //if modal is shown, remove the keydown event listener so that snake doesn't move
+      $("#alertModal").on("shown.bs.modal", function () {
+        window.removeEventListener("keydown", pressedKey);
+      });
+      //when modal hides, reset every variable and add keydown event listener again
+      $("#alertModal").on("hidden.bs.modal", function () {
+        context.clearRect(0, 0, 500, 500);
+        score.innerText = 0;
+        window.addEventListener("keydown", pressedKey);
+        reset();
+      });
+      modalBtn.addEventListener("click", () => {
+        context.clearRect(0, 0, 500, 500);
+        score.innerText = 0;
+      });
+    }, 1000);
+  }
 }
 
 //Random Obstacle Functions
@@ -399,3 +399,22 @@ function main() {
     score.innerText = totalTail;
   }, intervalDuration);
 }
+
+let modalGif = function () {
+  fetch('https://api.giphy.com/v1/gifs/search?q=snake&api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN&limit=1')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      console.log(response.data[0]);
+      let gifContainer = document.getElementById("gif-container");
+      gifContainer.innerHTML = '';
+
+      let gifImage = document.createElement('img');
+      gifImage.setAttribute('src', response.data[0].images.fixed_height.url);
+
+      gifContainer.appendChild(gifImage);
+    })
+}
+
+modalGif();
