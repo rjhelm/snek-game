14f82cb65cd2fc0611aba3fe23f99b0a149c4f77
rjhelm <77085453+rjhelm@users.noreply.github.com>
2021-03-11 17:19:13 -0700

    small additions and cleaned up script code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Global Variables
+// Global Variables for HTML Documents
 var canvas = document.getElementById("snakeCanvas");
 var context = canvas.getContext("2d");
 var score = document.getElementById("score");
@@ -8,6 +8,9 @@ var resumeBtn = document.getElementById("resumeBtn");
 var modalBtn = document.getElementById("modalBtn");
 var food = document.getElementById("food");
 var randomObstacle = document.getElementById("randomObstacle");
+var highScores = document.getElementById("hs-title");
+
+// Global Variables for Game Functions
 var snakeHeadX,
   snakeHeadY,
   foodX,
@@ -35,6 +38,10 @@ var playing, gameStarted;
 var boundaryCollision;
 var tail0;
 
+// Array for High Scores
+var userHigh = [];
+
+// Listen for click on Start Button
 startBtn.addEventListener("click", startGame);
 
 // Reset values of variables to starting values]
@@ -51,12 +58,11 @@ function reset() {
   ySpeed = 0;
   snakeHeadX = 0;
   snakeHeadY = 0;
-  pauseBtn.style.backgroundColor = "#fff";
-  resumeBtn.style.backgroundColor = "#fff";
   (playing = false), (gameStarted = false);
   boundaryCollision = false;
 }
-// Start the game 
+
+// Start the game
 function startGame() {
   reset();
   gameStarted = true;
@@ -66,37 +72,14 @@ function startGame() {
   main();
 }
 
-// Testing this function, but may be removed for different feature
-function pauseGame() {
-  window.clearInterval(gameInterval);
-  window.clearInterval(randObstacleInterval);
-  pauseBtn.style.backgroundColor = "#ccc";
-  resumeBtn.style.backgroundColor = "#fff";
-  playing = false;
-}
-
-// Part of pause testing
-function resumeGame() {
-  main();
-  pauseBtn.style.backgroundColor = "#fff";
-  resumeBtn.style.backgroundColor = "#ccc";
-  playing = true;
-}
-
-//EventListener to check which arrow key is pressed
+// Listens for arrow keys to be pressed for snake direction
 window.addEventListener("keydown", pressedKey);
 
-// will be fixed and updated 
+// Key press function
 function pressedKey() {
-  if (event.keyCode === 32 && gameStarted) {
-    if (playing) {
-      pauseGame();
-    } else {
-      resumeGame();
-    }
-  } else {
-    previousDir = direction;
+  if ((previousDir = direction)) {
     directionVar = event.key.replace("Arrow", "");
+
     changeDirection();
   }
 }
@@ -142,7 +125,7 @@ function changeDirection() {
   }
 }
 
-// Our Collision function to check boundaries and end game if hit
+// Checks for collision with  the snake body or canvas boundary
 function checkCollision() {
   let tailCollision = false,
     randObstacleCollision = false;
@@ -168,13 +151,17 @@ function checkCollision() {
   }
   return tailCollision || boundaryCollision || randObstacleCollision;
 }
-// Creates a random location to place food and obstacle
+
+// Randomly generates a coordinate for food and obstacle
 function generateCoordinates() {
   let xCoordinate = Math.floor(Math.random() * (max - min) + min) * scale;
   let yCoordinate = Math.floor(Math.random() * (max - min) + min) * scale;
   return { xCoordinate, yCoordinate };
 }
 
+// Snake //
+
+// Function that draws the snake head on the canvas
 function drawSnakeHead(color) {
   context.beginPath();
   context.arc(
@@ -253,6 +240,7 @@ function drawSnakeHead(color) {
   context.fill();
 }
 
+// Function draws tail onto canvas
 function drawSnakeTail() {
   let tailRadius = scale / 4;
   for (i = 0; i < tail.length; i++) {
@@ -297,7 +285,7 @@ function moveSnakeBack() {
   drawSnakeTail();
 }
 
-//display snake
+//display snake and if a collision occurs end the game and produce a modal to inform user and show final score
 function drawSnake() {
   drawSnakeHead("#7d4350");
   drawSnakeTail();
@@ -330,18 +318,28 @@ function drawSnake() {
   }
 }
 
-//Random Obstacle Functions
+// Random Obstacle //
+
+// Function to produce another obstacle for the user and increase the difficulty
 function randObstaclePosition() {
   let randomObstacle = generateCoordinates();
   randomObstacleX = randomObstacle.xCoordinate;
   randomObstacleY = randomObstacle.yCoordinate;
 }
 
+// Function draws the obstacle on the canvas
 function drawRandomObstacle() {
-  context.drawImage(randomObstacle, randomObstacleX, randomObstacleY, scale, scale);
+  context.drawImage(
+    randomObstacle,
+    randomObstacleX,
+    randomObstacleY,
+    scale,
+    scale
+  );
 }
 
 //Food//
+
 //generate random food position within canvas boundaries
 function foodPosition() {
   let food = generateCoordinates();
@@ -355,12 +353,17 @@ function drawFood() {
 }
 
 //Main Game Functions
+
+// Function to ensure food and obstacle generate and are drawn in different locations
 function checkSamePosition() {
   if (foodX == randomObstacleX && foodY == randomObstacleY) {
     randObstaclePosition();
   }
   for (let i = 0; i < tail.length; i++) {
-    if (randomObstacleX === tail[i].tailX && randomObstacleY === tail[i].tailY) {
+    if (
+      randomObstacleX === tail[i].tailX &&
+      randomObstacleY === tail[i].tailY
+    ) {
       randObstaclePosition();
       break;
     }
@@ -400,22 +403,24 @@ function main() {
   }, intervalDuration);
 }
 
-//api to display snake gif after game ends
+// Serer-Side Api fetch so the user see's a gif in the modal at the end of the game
 let modalGif = function () {
-  fetch('https://api.giphy.com/v1/gifs/search?q=snake&api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN&limit=1')
+  fetch(
+    "https://api.giphy.com/v1/gifs/search?q=snake&api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN&limit=1"
+  )
     .then(function (response) {
       return response.json();
     })
     .then(function (response) {
       console.log(response.data[0]);
       let gifContainer = document.getElementById("gif-container");
-      gifContainer.innerHTML = '';
+      gifContainer.innerHTML = "";
 
-      let gifImage = document.createElement('img');
-      gifImage.setAttribute('src', response.data[0].images.fixed_height.url);
+      let gifImage = document.createElement("img");
+      gifImage.setAttribute("src", response.data[0].images.fixed_height.url);
 
       gifContainer.appendChild(gifImage);
-    })
-}
+    });
+};
 
 modalGif();
